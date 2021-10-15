7bae6040612cfeae738dbda7d678fe58221d41ee
Harvey <64809348+harv-bala@users.noreply.github.com>
2021-10-15 14:10:32 +0100

    Split functions into CRUD operations
    To help readability as well as ease of use later in the project, the 'execute_query' and 'execute_read_query' have been split into separate Create, Read, Update, Delete methods. Corresponding SQL string patterns have also been added.
    
    The db_path attribute was updated to reflect the new path to the folder after file restructuring.
    
    Status 0 now means Down, and status 1 means Live.

--- main/db/db_helper.py
+++ main/db/db_helper.py
@@ -6,12 +6,12 @@ class DBHelper():
     def __init__(self) -> None:
         # Set the location of the database, as well as the filename. The Path class from pathlib automatically
         # corrects a path based on the OS. On Windows, this would change the '/' to a '\'.
-        self.db_path = Path('db/websiteDB.db')
+        self.db_path = Path('main/db/websiteDB.db')
 
         # Set the SQL pattern for creation of the 'websites' table. Status is set as an integer because
         # SQLite does not have a Boolean data type, so 0 and 1 act as True and False.
         # 'domain' refers to the domain name of the website, e.g. https://www.google.com
-        # 'status' refers to whether the website is live or down. 0 = Live, 1 = Down
+        # 'status' refers to whether the website is live or down. 0 = Down, 1 = Live
         self.create_table_sql = '''CREATE TABLE IF NOT EXISTS websites (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     domain TEXT NOT NULL,
@@ -26,38 +26,84 @@ class DBHelper():
                                         ("{}", {});
                                     '''
 
+        # Read record by its domain name, used when getting a website's status
+        self.read_record_sql = 'SELECT * FROM websites WHERE domain="{}"'
+
+        # If a website's status changes, this SQL will change the status for the
+        # domain name given
+        self.update_record_sql = '''UPDATE websites
+                                        SET status={}
+                                    WHERE domain="{}"
+                                    '''
+
+        # Removes a record based on matching domain name
+        self.delete_record_sql = 'DELETE FROM websites WHERE domain="{}"'
+
     def create_connection(self):
         connection = None
         try:
             # Creating a connection to the database, passing in the path attribute
+            # The Path object much be cast to a strig when passed to the connect function
             connection = sqlite3.connect(str(self.db_path))
             print('Connection to DB established')
         except Error as e:
             print(f'The following error occurred: {e}')
         return connection
 
-    def execute_query(self, query):
-        # Establish connection to DB
+    def create_table(self):
         connection = self.create_connection()
-        # The cursor allows traversal over the records in the DB. It is used to execute SQL queries on the DB.
+        # The cursor allows iteration over the records in the database, and handles the execution
+        # of any SQL
         cursor = connection.cursor()
         try:
-            cursor.execute(query)
-            # Commit (save) the changes made
+            cursor.execute(self.create_table_sql)
+            # After making changes, they are committed (saved)
             connection.commit()
-            print('Query executed')
         except Error as e:
             print(f'The following error occurred: {e}')
 
-    def execute_read_query(self, query):
-        # Establish connection to DB
+    # ---------- CREATE ----------
+    def create_record(self, domain):
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        try:
+            # .format() is used to insert the arguments into the pre-set SQL pattern
+            cursor.execute(self.insert_record_sql.format(domain, 1))
+            connection.commit()
+            print('Created new record')
+        except Error as e:
+            print(f'The following error occurred: {e}')
+
+    # ---------- READ ----------
+    def read_record(self, domain):
         connection = self.create_connection()
         cursor = connection.cursor()
         result = None
         try:
-            cursor.execute(query)
-            # fetchall() returns all of the rows that matched the query we executed with the cursor
+            cursor.execute(self.read_record_sql.format(domain))
             result = cursor.fetchall()
             return result
+        except Error as e:
+            print(f'The following error occurred: {e}')
+
+    # ---------- UPDATE ----------
+    def update_record(self, domain, status):
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        try:
+            cursor.execute(self.update_record_sql.format(status, domain))
+            connection.commit()
+            print('Updated record')
+        except Error as e:
+            print(f'The following error occurred: {e}')
+
+    # ---------- DELETE ----------
+    def delete_record(self, domain):
+        connection = self.create_connection()
+        cursor = connection.cursor()
+        try:
+            cursor.execute(self.delete_record_sql.format(domain))
+            connection.commit()
+            print('Deleted record')
         except Error as e:
             print(f'The following error occurred: {e}')
