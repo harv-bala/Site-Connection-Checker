02a56c8400bc9832ab34d6a84bad2d068b8ec2aa
Harvey <64809348+harv-bala@users.noreply.github.com>
2021-10-17 15:29:08 +0100

    Renamed domain attribute as url
    For the StatusChecker class, the attribute 'domain' is now more appropriately named 'url'.
    
    Added comments to the check_status() method.

--- main/status/status_checker.py
+++ main/status/status_checker.py
@@ -1,16 +1,22 @@
 import requests
 
 class StatusChecker():
-    def __init__(self, domain) -> None:
-        self.domain = domain
+    def __init__(self, url) -> None:
+        self.url = url
         self.status = None
 
     def check_status(self):
         try:
-            response = requests.get(self.domain)
+            # Send HTTP GET request to the given url
+            response = requests.get(self.url)
+            # Response codes of the format 5xx are server errors. This would indicate
+            # that the website is down, hence status is set to false (Down)
             if str(response.status_code)[0] == '5':
                 self.status = False
             else:
+                # Typically, a 200 OK code will be sent back from a page, however client errors can
+                # also occurr, of the format 4xx, such as a 404 (page requested wasn't found). This doesn't suggest
+                # that there's an issue with the server, however, so the status is still set to True (Live)
                 self.status = True
         except Exception as e:
             print(f'The following exception was caught: {e}')
