8a746d8e67499bab273e50687376cbad4af998b9
Harvey <64809348+harv-bala@users.noreply.github.com>
2021-11-25 13:08:39 +0000

    Overhaul db_helper.py for PEP8
    Complete restructuring of db_helper.py to comply with PEP8 where it makes sense. No functionality changes.

--- main/db/db_helper.py
+++ main/db/db_helper.py
@@ -2,23 +2,29 @@ import sqlite3
 from sqlite3 import Error
 from pathlib import Path
 
+
 class DBHelper():
+    '''executes database operations for easier higher-level use'''
     def __init__(self) -> None:
-        # Set the location of the database, as well as the filename. The Path class from pathlib automatically
-        # corrects a path based on the OS. On Windows, this would change the '/' to a '\'.
+        # Set the location of the database, as well as the filename. The Path
+        # class from pathlib automatically corrects a path based on the OS.
+        # On Windows, this would change the '/' to a '\'.
         self.db_path = Path('db/websiteDB.db')
 
-        # Set the SQL pattern for creation of the 'websites' table. Status is set as an integer because
-        # SQLite does not have a Boolean data type, so 1 and 0 act as True and False.
-        # 'domain' refers to the domain name of the website, e.g. https://www.google.com
-        # 'status' refers to whether the website is live or down. 0 = Down, 1 = Live
+        # Set the SQL pattern for creation of the 'websites' table.
+        # Status is set as an integer because SQLite does not have a Boolean
+        # data type, so 1 and 0 act as True and False. 'domain' refers to
+        # the domain name of the website, e.g. https://www.google.com
+        # 'status' refers to whether the website is live or down.
+        # 0 = Down, 1 = Live
         self.create_table_sql = '''CREATE TABLE IF NOT EXISTS websites (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     domain TEXT NOT NULL,
                                     status INTEGER NOT NULL
                                 );'''
 
-        # General SQL pattern for inserting a record. Using .format(), the gaps '{}' can be filled in
+        # General SQL pattern for inserting a record.
+        # Using .format(), the gaps '{}' can be filled in
         # with whatever values are needed.
         self.insert_record_sql = '''INSERT INTO
                                         websites (domain, status)
@@ -31,8 +37,8 @@ class DBHelper():
 
         self.read_all_sql = 'SELECT * FROM websites'
 
-        # If a website's status changes, this SQL will change the status for the
-        # domain name given
+        # If a website's status changes, this SQL will
+        # change the status for the domain name given
         self.update_record_sql = '''UPDATE websites
                                         SET status={}
                                     WHERE domain="{}"
@@ -44,40 +50,45 @@ class DBHelper():
         self.create_table()
 
     def create_connection(self):
+        '''create a connection to the database'''
         connection = None
         try:
-            # Creating a connection to the database, passing in the path attribute
-            # The Path object much be cast to a strig when passed to the connect function
+            # Creating a connection to the database,
+            # passing in the path attribute
+            # The Path object much be cast to a strig
+            # when passed to the connect function
             connection = sqlite3.connect(str(self.db_path))
-        except Error as e:
-            print(f'The following error occurred: {e}')
+        except Error as error:
+            print(f'The following error occurred: {error}')
         return connection
 
     def create_table(self):
+        '''create a cursor to access records in the database'''
         connection = self.create_connection()
-        # The cursor allows iteration over the records in the database, and handles the execution
-        # of any SQL
+        # The cursor allows iteration over the records in the database,
+        # and handles the execution of any SQL
         cursor = connection.cursor()
         try:
             cursor.execute(self.create_table_sql)
             # After making changes, they are committed (saved)
             connection.commit()
-        except Error as e:
-            print(f'The following error occurred: {e}')
+        except Error as error:
+            print(f'The following error occurred: {error}')
 
-    # ---------- CREATE ----------
     def create_record(self, domain):
+        '''create a new record in the database'''
         connection = self.create_connection()
         cursor = connection.cursor()
         try:
-            # .format() is used to insert the arguments into the pre-set SQL pattern
+            # .format() is used to insert the arguments
+            # into the pre-set SQL pattern
             cursor.execute(self.insert_record_sql.format(domain, 1))
             connection.commit()
-        except Error as e:
-            print(f'The following error occurred: {e}')
+        except Error as error:
+            print(f'The following error occurred: {error}')
 
-    # ---------- READ ----------
     def read_record(self, domain):
+        '''read information from a record in the database'''
         connection = self.create_connection()
         cursor = connection.cursor()
         result = None
@@ -85,10 +96,11 @@ class DBHelper():
             cursor.execute(self.read_record_sql.format(domain))
             result = cursor.fetchall()
             return result
-        except Error as e:
-            print(f'The following error occurred: {e}')
+        except Error as error:
+            print(f'The following error occurred: {error}')
 
     def read_all_records(self):
+        '''read information from all records in the database'''
         connection = self.create_connection()
         cursor = connection.cursor()
         result = None
@@ -96,28 +108,28 @@ class DBHelper():
             cursor.execute(self.read_all_sql)
             result = cursor.fetchall()
             return result
-        except Error as e:
-            print(f'The following error occurred: {e}')
+        except Error as error:
+            print(f'The following error occurred: {error}')
 
-    # ---------- UPDATE ----------
     def update_record(self, domain, status):
+        '''update information in a record'''
         connection = self.create_connection()
         cursor = connection.cursor()
         try:
             cursor.execute(self.update_record_sql.format(status, domain))
             connection.commit()
-        except Error as e:
-            print(f'The following error occurred: {e}')
+        except Error as error:
+            print(f'The following error occurred: {error}')
 
-    # ---------- DELETE ----------
     def delete_record(self, domain):
+        '''delete a record from the database'''
         connection = self.create_connection()
         cursor = connection.cursor()
         if self.read_record(domain):
             try:
                 cursor.execute(self.delete_record_sql.format(domain))
                 connection.commit()
-            except Error as e:
-                print(f'The following error occurred: {e}')
+            except Error as error:
+                print(f'The following error occurred: {error}')
         else:
             print(f"The website '{domain}' does not exist")
