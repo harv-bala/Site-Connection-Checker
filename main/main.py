ed63ef44dd5d8c066b002806ff62a2a32ab449eb
Harvey <64809348+harv-bala@users.noreply.github.com>
2021-11-25 13:12:09 +0000

    Overhaul main.py for PEP8
    Complete restructuring of main.py to comply with PEP8 where appropriate. Main changes are inclusion of docstrings and line shortening. No functionality changes.

--- main/main.py
+++ main/main.py
@@ -5,85 +5,118 @@ from pathlib import Path
 from db.db_helper import DBHelper
 from status.status_checker import StatusChecker
 
-def validate_url(url):
-    statChecker = StatusChecker(url)
-    statChecker.check_status()
 
-def add_to_database(url):
+def validate_url(url: str):
+    '''check url is in the correct format'''
+    stat_checker = StatusChecker(url)
+    stat_checker.check_status()
+
+
+def add_to_database(url: str):
+    '''add a website to the database'''
     validate_url(url)
-    webDB = DBHelper()
-    webDB.create_record(url)
+    web_db = DBHelper()
+    web_db.create_record(url)
 
-def remove_from_database(url):
-    webDB = DBHelper()
-    webDB.delete_record(url)
 
-def get_interval():
+def remove_from_database(url: str):
+    '''remove a website from the database'''
+    web_db = DBHelper()
+    web_db.delete_record(url)
+
+
+def get_interval() -> int:
+    '''return the time interval stored in the settings file'''
     filepath = str(Path('settings.json'))
-    with open(filepath, 'r') as f:
-        data = json.load(f)
-    interval = data['settings'][0]['interval']
-    return int(interval)
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    time_interval = data['settings'][0]['interval']
+    return int(time_interval)
 
-def set_interval(interval):
+
+def set_interval(time_interval: int):
+    '''set (overwrite) the time interval stored in the settings file'''
     filepath = str(Path('settings.json'))
-    with open(filepath, 'r') as f:
-        data = json.load(f)
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
 
-    data['settings'][0]['interval'] = str(interval)
+    data['settings'][0]['interval'] = str(time_interval)
 
-    with open(filepath, 'w') as f:
-        json.dump(data, f)
+    with open(filepath, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
+
 
 def print_results():
-    webDB = DBHelper()
-    results = webDB.read_all_records()
-    for result in results:
-        if result[2] == 1:
-            print(f'{result[1]:<34} [OK]')
-        elif result[2] == 0:
-            print(f'{result[1]:<34} [ERROR]')
+    '''print formatted website statuses'''
+    web_db = DBHelper()
+    results = web_db.read_all_records()
+    if results:
+        for result in results:
+            if result[2] == 1:
+                print(f'{result[1]:<34} [OK]')
+            elif result[2] == 0:
+                print(f'{result[1]:<34} [ERROR]')
+
 
 def print_sites():
-    webDB = DBHelper()
-    results = webDB.read_all_records()
+    '''print list of all stored websites'''
+    web_db = DBHelper()
+    results = web_db.read_all_records()
     print('Current stored sites: \n')
-    for result in results:
-        print(result[1])
+    if results:
+        for result in results:
+            print(result[1])
+
 
 def print_sites_verbose():
-    webDB = DBHelper()
-    results = webDB.read_all_records()
-    print(f'Here are the sites you currently have stored. There are {len(results)} sites: \n')
-    for i in range(len(results)):
-        print(f'Site {i + 1}: {results[i][1]}')
+    '''print a formatted, verbose list of stored websites'''
+    web_db = DBHelper()
+    results = web_db.read_all_records()
+    if results:
+        print(
+            'Here are the sites you currently have stored. '
+            f'There are {len(results)} sites: \n')
+        for i, j in enumerate(results):
+            print(f'Site {i + 1}: {j[1]}')
+    else:
+        pass
+
 
 def main():
-    webDB = DBHelper()
-    interval = get_interval()
+    '''continuously check status of stored websites unless interrupted'''
+    web_db = DBHelper()
+    time_interval = get_interval()
     print_results()
 
     try:
         while True:
-            t.sleep(interval)
-            records = webDB.read_all_records()
-            for record in records:
-                statChecker = StatusChecker(record[1])
-                statChecker.check_status()
-                currentStatus = statChecker.get_status()
-                if currentStatus != record[2]:
-                    webDB.update_record(record[1], currentStatus)
-                    print('STATUS UPDATED FOR ONE OR MORE WEBSITES')
-            print_results()
+            t.sleep(time_interval)
+            records = web_db.read_all_records()
+            if records:
+                for record in records:
+                    stat_checker = StatusChecker(record[1])
+                    stat_checker.check_status()
+                    current_status = stat_checker.get_status()
+                    if current_status != record[2]:
+                        web_db.update_record(record[1], current_status)
+                        print('STATUS UPDATED FOR ONE OR MORE WEBSITES')
+                print_results()
     except KeyboardInterrupt:
         print('PROGRAM STOPPED')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Site Connection Checker')
     subparser = parser.add_subparsers(dest='command')
-    database = subparser.add_parser('database', help='Perform database operations')
-    interval = subparser.add_parser('interval', help='Change the check time interval')
-    listSites = subparser.add_parser('list', help='List all websites stored in the database')
+    database = subparser.add_parser(
+                                    'database',
+                                    help='Perform database operations')
+    interval = subparser.add_parser(
+                                    'interval',
+                                    help='Change the check time interval')
+    listSites = subparser.add_parser(
+                                    'list',
+                                    help='List all websites in the database')
     start = subparser.add_parser('start', help='Start the program')
 
     db_group = database.add_mutually_exclusive_group()
@@ -91,9 +124,19 @@ if __name__ == '__main__':
     db_group.add_argument('-r', '--remove', metavar='', help='Remove from DB')
 
     list_group = listSites.add_mutually_exclusive_group()
-    list_group.add_argument('-v', '--verbose', help='Output a more verbose list of websites', action='store_true')
+    list_group.add_argument(
+                            '-v',
+                            '--verbose',
+                            help='Output a more verbose list of websites',
+                            action='store_true')
 
-    interval.add_argument('-c', '--change', metavar='', type=int, help='Change the check time interval', required=True)
+    interval.add_argument(
+                        '-c',
+                        '--change',
+                        metavar='',
+                        type=int,
+                        help='Change the check time interval',
+                        required=True)
 
     args = parser.parse_args()
 
